0febbb437afc3759dc2a65f8daeff64b1a6faa4c
Ben Stevenson <bcs5@st-andrews.ac.uk>
2016-10-03 13:34:41 +0100

    Adding a couple of packages.

--- analysis/code/packages.R
+++ analysis/code/packages.R
@@ -1,5 +1,5 @@
-## Add any new packages to the `pkgs' vector.
-pkgs <- "secr"
+## Add any new packages to the `pkgs' vector in alphabetical order.
+pkgs <- c("knitr", "secr", "testthat", "xtable")
 for (i in pkgs){
     if (!require(i, character.only = TRUE)){
         install.packages(i, repos = "http://star-www.st-andrews.ac.uk/cran/")
