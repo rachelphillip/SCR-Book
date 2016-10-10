4e4b4a9f92ae5bdce354189838832bb24c34b4e5
Ben Stevenson <bcs5@st-andrews.ac.uk>
2016-10-10 13:44:16 +0100

    Moving code from inst/ to inst/code/.

---
+++ scrmlebook/inst/code/example.R
@@ -0,0 +1,12 @@
+## Code to fit a simple example SCR model, taken from the secr.fit() help file.
+library(secr)
+
+## Always load in data with a relative (rather than full) file specification.
+## That is, use "../data/data-file" instead of "/home/ben/GitHub/analysis/data/data-file"
+load("../../data/example-data.RData")
+
+## Fitting model.
+example.fit <- secr.fit(detections, buffer = 100, trace = FALSE)
+
+## Saving results. Again, use a relative file specification.
+save(example.fit, file = "../../data/example-fits.RData")
