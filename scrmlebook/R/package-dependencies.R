1059900ce813d94f29bbd5c82f39c9b006b90e91
Ben Stevenson <bcs5@st-andrews.ac.uk>
2016-10-11 10:45:05 +0100

    Writing all docs with roxygen.

--- scrmlebook/R/package-dependencies.R
+++
@@ -1,7 +0,0 @@
-## Roxygen stuff for NAMESPACE.
-#' @importFrom fields image.plot rdist
-#' @importFrom graphics image lines plot polygon text
-#' @importFrom grDevices dev.off png
-#' @import secr
-#' @importFrom stats plogis
-NULL
---
+++ scrmlebook/R/scrmlebook.R
@@ -0,0 +1,25 @@
+#' Utility package for book SCR by maximum likelihood, with R
+#'
+#' Functions and datasets to go with the book 'Spatial Capture-Recapture by Maximum Likelihood, with R'.
+#'
+#' @author
+#' David Borchers et al.
+#'
+#' @name scrmlebook
+#' @docType package
+#' 
+#' @importFrom fields image.plot rdist
+#' @importFrom graphics image lines plot polygon text
+#' @importFrom grDevices dev.off png
+#' @import secr
+#' @importFrom stats plogis
+#' 
+#' @references
+#' Borchers, D. L., Stevenskon, B. C., Rexstad, E., Howe, E. J., and Distiller, G. Spatial Capture-Recapture by Maximum Likelihood, with R
+#'
+#' @keywords
+#' Spatial capture-recapture
+#'
+#' @examples
+#' ## To be added.
+NULL
